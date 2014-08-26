cd6bf99336ae89018fa90debf58764541b76cf15
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2014-08-26 22:57:29 +0100

    Added thumbnails categories

--- src/app/app.js
+++ src/app/app.js
@@ -10,8 +10,7 @@ angular.module("OnlineStore", ["ngRoute"])
     $routeProvider
         .when("/home",
         {
-            templateUrl : "home/home.tpl.html",
-            controller  : "HomeCtrl"
+            templateUrl : "home/home.tpl.html"
         })
         .otherwise(
         {
@@ -25,28 +24,36 @@ angular.module("OnlineStore", ["ngRoute"])
 .constant("PRODUCT_CATEGORIES",
     [
         {
-            name : "All products",
-            id   : ''
+            name : "Vinhos"/*,
+            img  : "http://www.cafeportugal.pt/resources/3/image/amendoamoncorvo002.JPG"*/
         },
         {
-            name : "Wines",
-            id   : 'wine'
+            name : "Azeite"/*,
+            img  : "/src/assets/img/home/azeite.jpg"*/
         },
         {
-            name : "Olive Oil",
-            id   : 'olive_oil'
+            name : "Queijo"/*,
+            img  : "http://1.bp.blogspot.com/-mPlqP23acfM/UbdinxrHmVI/AAAAAAAAOi4/_JpaTeqXRzM/s1600/FLOR+DA+CARDANHA.jpg"*/
         },
         {
-            name : "Cheese",
-            id   : 'cheese'
+            name : "Pão"/*,
+            img  : "http://2.bp.blogspot.com/-PU5YgNAHeNs/TweJScFDKyI/AAAAAAAADyY/sng5WNQoIJw/s1600/carvi%25C3%25A7ais+p%25C3%25A3o.jpg"*/
         },
         {
-            name : "Bread",
-            id   : 'bread'
+            name : "Fumeiro"/*,
+            img  : "http://jornal.netbila.net/images/stories/imagens/2011/gastronomia/o_que_se_come/alheiras.jpg"*/
         },
         {
-            name : "Fumeiro",
-            id   : 'fumeiro'
+            name : "Mel"/*,
+            img  : "http://www.mulhernamoda.com/wp-content/uploads/2013/09/beneficios-do-mel_02.jpg"*/
+        },
+        {
+            name : "Licor"/*,
+            img  : "http://armazemdabebida.com.br/loja/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/l/i/licor-43-1.jpg"*/
+        },
+        {
+            name : "Artesanato"/*,
+            img  : "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQK8K_ZYecOKyb4RILtuTk034jFQGLHFKWyxnjnSD6mURmr7Nhjpg"*/
         }
     ]
 )
@@ -56,55 +63,55 @@ angular.module("OnlineStore", ["ngRoute"])
  */
 .constant("PRODUCTS", [
 
-    /* WINES */
+    /* Vinhos */
     {
-        category    : "wine",
+        category    : "vinho",
         name        : "Cistus Tinto",
         description : "bla bla bla",
         retailer    : "Quinta do Vale da Perdiz – Sociedade Agrícola, Lda."
     },
     {
-        category    : "wine",
+        category    : "vinho",
         name        : "Cistus Tinto Reserve",
         description : "bla bla bla",
         retailer    : "Quinta do Vale da Perdiz – Sociedade Agrícola, Lda."
     },
 
-    /* OLIVE OIL */
+    /* Azeite */
     {
-        category    : "olive_oil",
-        name        : "Olive Oil",
+        category    : "azeite",
+        name        : "Azeite",
         description : "bla bla bla",
         retailer    : "Bazar Regional"
     },
     {
-        category    : "olive_oil",
-        name        : "Olive Oil",
+        category    : "azeite",
+        name        : "Azeite",
         description : "bla bla bla",
         retailer    : "Arte Sabor e Douro"
     },
 
     /* CHEESE */
     {
-        category    : "cheese",
+        category    : "queijo",
         name        : "Queijo de Cabra",
         description : "bla bla bla",
         retailer    : "Bazar Regional"
     },
     {
-        category    : "cheese",
+        category    : "queijo",
         name        : "Queijo de Cabra Curado",
         description : "bla bla bla",
         retailer    : "Bazar Regional"
     },
     {
-        category    : "cheese",
+        category    : "queijo",
         name        : "Queijo de Ovelha",
         description : "bla bla bla",
         retailer    : "Bazar Regional"
     },
     {
-        category    : "cheese",
+        category    : "queijo",
         name        : "Queijo de Ovelha Curado",
         description : "bla bla bla",
         retailer    : "Bazar Regional"
