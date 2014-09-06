2ad7fec8bf92dda781b456f76250c462911e5ff1
ruiafonsopereira <ruiafonsopereira@gmail.com>
2014-09-06 15:44:48 +0100

    Resource removed

--- src/app/app.js
+++ src/app/app.js
@@ -1,7 +1,7 @@
 /*jslint browser: true, devel: true, nomen: true, plusplus: true, vars: true, white: true */
 /*global angular*/
 
-angular.module("OnlineStore", ["ngRoute", "ngResource"])
+angular.module("OnlineStore", ["ngRoute"])
 
 .config(["$routeProvider", function($routeProvider)
 {
