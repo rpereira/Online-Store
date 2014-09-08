10aab7275d196d79a83f960845b0254bd55a14ce
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2014-09-09 00:14:11 +0100

    Product details prototype

--- src/app/products/DetailsController.js
+++ src/app/products/DetailsController.js
@@ -3,10 +3,15 @@
 
 angular.module("OnlineStore")
 
-.controller("DetailsController",
-            ["$scope", "$routeParams",
-            function($scope, $routeParams)
-            {
-                $scope.product_id = $routeParams.product_id;
-            }
-]);
+.controller("DetailsController", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http)
+{
+    $scope.category_id = $routeParams.category_id;
+    $scope.product_id = $routeParams.product_id;
+
+    $http
+        .get("products/" + $scope.category_id + "/" + $scope.product_id + ".json")
+        .then(function(res)
+        {
+            $scope.product = res.data;
+        });
+}]);
