2b60723012ebd2f2d683e7d485cec5d6e7791a2b
Rui Afonso Pereira <rpereira@users.noreply.github.com>
2014-09-10 16:03:50 +0100

    Code legibility optimized

--- src/app/products/DetailsController.js
+++ src/app/products/DetailsController.js
@@ -6,10 +6,10 @@ angular.module("OnlineStore")
 .controller("DetailsController", ["$scope", "$routeParams", "$http", function($scope, $routeParams, $http)
 {
     $scope.category_id = $routeParams.category_id;
-    $scope.product_id = $routeParams.product_id;
+    $scope.product_id  = $routeParams.product_id;
 
     $http
-        .get("products/" + $scope.category_id + "/" + $scope.product_id + ".json", { cache: true})
+        .get("products/" + $scope.category_id + "/" + $scope.product_id + ".json", { cache: true })
         .then(function(res)
         {
             $scope.product = res.data;
